a952d45e2cd0fde9e0f7377a2489a2de2be0db71
MaartenH91 <71693543+MaartenH91@users.noreply.github.com>
2023-11-18 16:58:57 +0100

    Update DatabaseSource.ts

--- database/DatabaseSource.ts
+++ database/DatabaseSource.ts
@@ -52,7 +52,6 @@ export const AppDataSource = new DataSource({
   ssl:
     process.env.ENV === "PRODUCTION"
       ? {
-          require: true, 
           rejectUnauthorized: false,
         }
       : false,
